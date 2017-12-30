15990cbd5b4384cbe53ada429e4901b0e28ff380
e <15151869028@163.com>
2017-12-30 17:03:03 +0800

    publish

--- other/H5/js/functions.js
+++ other/H5/js/functions.js
@@ -16,7 +16,7 @@ $(function () {
     gardenCtx.globalCompositeOperation = "lighter";
     garden = new Garden(gardenCtx, gardenCanvas);
 	
-	$("#content").css("width", $loveHeart.width() + $("#code").width());
+	$("#content").css("width", Math.max($loveHeart.width(), $("#code").width()));
 	$("#content").css("height", Math.max($loveHeart.height(), $("#code").height()));
 	$("#content").css("margin-top", Math.max(($window.height() - $("#content").height()) / 2, 10));
 	$("#content").css("margin-left", Math.max(($window.width() - $("#content").width()) / 2, 10));
@@ -132,7 +132,9 @@ function adjustWordsPosition() {
 }
 
 function adjustCodePosition() {
-	$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
+	//$('#code').css("margin-top", ($("#garden").height() - $("#code").height()) / 2);
+	$('#code').css("margin-top", 10);
+	$('#code').css("margin-left", 5);
 }
 
 function showLoveU() {
